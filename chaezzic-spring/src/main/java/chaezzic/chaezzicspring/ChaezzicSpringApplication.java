23a0bfa741bf25990d6875b1edffa35d4cc151bb
Sermadl <rlatpdms1014@gmail.com>
2023-11-14 10:41:17 +0900

    create project

---
+++ chaezzic-spring/src/main/java/chaezzic/chaezzicspring/ChaezzicSpringApplication.java
@@ -0,0 +1,14 @@
+package chaezzic.chaezzicspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaezzicSpringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ChaezzicSpringApplication.class, args);
+	}
+
+}
---
+++ chaezzic-spring/src/test/java/chaezzic/chaezzicspring/ChaezzicSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package chaezzic.chaezzicspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaezzicSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
