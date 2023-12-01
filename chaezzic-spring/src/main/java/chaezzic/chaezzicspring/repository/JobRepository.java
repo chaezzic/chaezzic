f762481383707d68d6092ac03ba3146b17ff17be
Sermadl <rlatpdms1014@gmail.com>
2023-12-01 16:46:31 +0900

    타임리프 공부

---
+++ chaezzic-spring/src/main/java/chaezzic/chaezzicspring/repository/JobRepository.java
@@ -0,0 +1,12 @@
+package chaezzic.chaezzicspring.repository;
+
+import chaezzic.chaezzicspring.domain.Job;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface JobRepository {
+
+    List<Job> findAll();
+    Optional<Job> findByJobTitle(String jobTitle);
+}
